f2ac5cd3e5eaa5c2e51a4d49a66a5b6311944aca
Andrius Milinis <milinis@gmail.com>
2023-04-08 13:05:07 +0300

    Initial commit

---
+++ 34_url_query/34_url_query.js
@@ -0,0 +1,12 @@
+//http://localhost:8080/?year=2019&month=fabrualy
+
+var http = require('http');
+var url = require('url')
+
+http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var q = url.parse(req.url, true).query;
+	var dates = q.year + ' ' + q.month;
+	res.write(dates);
+	res.end();
+}).listen(8080);
---
+++ 35_files_index_page/35_files_index_page.js
@@ -0,0 +1,12 @@
+//http://localhost:8080/?year=2019&month=fabrualy
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res){
+	fs.readFile('index.html', function(err, data){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		res.end();
+	})	
+}).listen(8080);
